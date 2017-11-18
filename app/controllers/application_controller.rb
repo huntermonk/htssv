2a39a860169404e2da3dde13eec93379366f81f7
Hunter Maximillion Monk <hunter.monk@gmail.com>
2017-11-17 21:05:48 -0800

    Added six examples

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+#  def setup
+#    @beginning = "Instead of saying "
+#    @middle = ", say"
+#    @end = "."
+#  end
+
   def hello
-    render html: ["Hi", "Another one!", "This is cray."].sample
+    render html: 
+      ["Instead of saying split, say bifurcate.",
+       "Instead of saying combine, say couple.",
+       "Instead of saying between 5 and 10, say not less than 5, but not more than 10.",
+       "Instead of saying there's a chance, say there's a non-zero possibility.",
+       "Instead of saying combine, say conflate.",
+       "Instead of saying explain, say unpack."
+    ].sample
   end
 end
