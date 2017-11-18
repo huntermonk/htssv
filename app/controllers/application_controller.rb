5ff5830db518b13c7576fe24896b73a7d69e3a7f
Hunter Maximillion Monk <hunter.monk@gmail.com>
2017-11-17 21:35:20 -0800

    Add quotes to the phrases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,12 @@ class ApplicationController < ActionController::Base
 
   def hello
     render html: 
-      ["Instead of saying split, say bifurcate.",
-       "Instead of saying combine, say couple.",
-       "Instead of saying between 5 and 10, say not less than 5, but not more than 10.",
-       "Instead of saying there's a chance, say there's a non-zero possibility.",
-       "Instead of saying combine, say conflate.",
-       "Instead of saying explain, say unpack."
+      ["Instead of saying \"split\", say \"bifurcate\".",
+       "Instead of saying \"combine\", say \"couple\".",
+       "Instead of saying \"between 5 and 10\", say \"not less than 5, but not more than 10\".",
+       "Instead of saying \"there's a chance\", say \"there's a non-zero possibility\".",
+       "Instead of saying \"mix\", say \"conflate\".",
+       "Instead of saying \"explain\", say \"unpack\"."
     ].sample
   end
 end
